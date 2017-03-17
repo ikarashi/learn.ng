4b8bcd7acf82b735b8d6f639fd22237375d79e45
ikarashi <mika.ikarashi@bizreach.co.jp>
2017-03-17 20:20:19 +0900

    足し算できた

--- src/components/number.button/number.button.component.ts
+++ src/components/number.button/number.button.component.ts
@@ -1,8 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, Input, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'calc-num-button',
   templateUrl: '../abstract.button/abstract.button.component.html',
   styleUrls: ['../abstract.button/abstract.button.component.scss']
 })
-export class NumberButtonComponent {}
+export class NumberButtonComponent implements OnInit {
+  @Input() private value: number;
+  @Input() @Output() public calc: any;
+
+  get text () : string {
+    return String(this.value);
+  }
+
+  public ngOnInit () : void {
+    console.log(this.text);
+  }
+
+  public onClick () : void {
+    this.calc.total += this.value;
+  }
+}
